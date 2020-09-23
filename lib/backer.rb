49f55d5b89ba581c95997a0724c56f241e152026
Matt Thomas <matthew.keith.thomas@gmail.com>
2020-09-23 16:36:44 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -6,7 +6,7 @@ class Backer
     @backed_projects = []
   end 
   
-  def back_project(projects)
-    @backed_projects << self 
+  def back_project(project)
+    @backed_projects << project
   end 
 end 
